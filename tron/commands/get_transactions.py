c5dcea247087cbafbfbbcda274f3150dc8545b10
xrisent <ormonov31261@gmail.com>
2023-11-20 12:02:21 +0600

    fixed getting all transactions

--- tron/commands/get_transactions.py
+++ tron/commands/get_transactions.py
@@ -14,15 +14,30 @@ def get_transactions(address, api_key, params={}):
 
     data = []
 
-    for transaction in response.json()['data']:
-        time = datetime.fromtimestamp(transaction['block_timestamp']/1000)
-        data.append({
-            'transaction_id': transaction['transaction_id'],
-            'from': transaction['from'],
-            'to': transaction['to'],
-            'value': transaction['value'],
-            'timestamp': transaction['block_timestamp'],
-            'time': '%s'%time,
-        })
+    def format_transactions(response_data):
+        for transaction in response_data:
+                time = datetime.fromtimestamp(transaction['block_timestamp']/1000)
+                data.append({
+                    'transaction_id': transaction['transaction_id'],
+                    'from': transaction['from'],
+                    'to': transaction['to'],
+                    'value': transaction['value'],
+                    'timestamp': transaction['block_timestamp'],
+                    'time': '%s'%time,
+                })
+    
+
+    while True:
+        response = requests.get(url, headers=headers, params=params)
+        response_data = response.json()['data']
+
+        format_transactions(response_data)
+
+        fingerprint = response.json()['meta'].get('fingerprint')
+        if fingerprint:
+            params['fingerprint'] = fingerprint
+        else:
+            break
+
     
     return data
