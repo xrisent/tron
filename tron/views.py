60fafa1b6948ada0f5e618a5f33b926861410cf0
xrisent <ormonov31261@gmail.com>
2023-11-27 10:20:52 +0600

    fixed more errors

--- tron/views.py
+++ tron/views.py
@@ -26,7 +26,7 @@ from core.settings import TRON_SETTINGS
 
 api_key = config('API_TRONGRID_KEY')
 api_key_chainalysis = config('CHAINALYSIS_API_KEY')
-PARAMS = config('PARAMS')
+PARAMS = json.loads(config('PARAMS'))
 
 @swagger_auto_schema(
     methods=['get'],
