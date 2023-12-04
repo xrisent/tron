60aa70c473df98ce9101248de9bf9a8674123b70
xrisent <ormonov31261@gmail.com>
2023-12-04 21:20:18 +0600

    fixed get_account_info

--- tron/commands/get_account_info.py
+++ tron/commands/get_account_info.py
@@ -15,21 +15,20 @@ async def get_info(address, api_key):
     for k, v in response.json().items():
         if k == 'totalTransactionCount':
             data['transactions_len'] = v
-        else:
-            data['transactions_len'] = 0
             
         if k == 'withPriceTokens':
             for dict in v:
                 if dict['tokenId'] == 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t':
                     # data['transactions_len'] = dict['transferCount']
                     data['balance'] = int(dict['balance']) / 1000000
-                else:
-                    data['balance'] = 0
-        else:
-            data['balance'] = 0
         
         if k == 'redTag':
             data['redTag'] = v
+    
+    if data['transactions_len'] == None:
+        data['transactions_len'] = 0
+    elif data['balance'] == None:
+        data['balance'] = 0
 
     return data
 
